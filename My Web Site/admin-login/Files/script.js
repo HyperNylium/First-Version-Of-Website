011756b9f68150e64f4a3b3ef0c4f25c9b55670d
HyperNylium <davidpivik123@gmail.com>
2022-10-18 21:57:06 -0400

    Update

---
+++ My Web Site/admin-login/Files/script.js
@@ -0,0 +1,22 @@
+function checkPassword()
+{
+	var password = document.getElementById('passwordBox');
+	var passwordText = password.value;
+	if(passwordText == "c0mput3rs") {
+		alert("Authentication Successful. Click > Ok < To Proceed");
+		window.location.href = ( "http://hypernylium.com/admin-login/ogngndgouysznlıuytfdcbnkufcvbjytfc");
+
+	}
+
+	alert("Authentication Failed, Click > Ok < To Try Again. ");
+		return false;
+	}
+
+
+
+	
+
+
+
+
+
